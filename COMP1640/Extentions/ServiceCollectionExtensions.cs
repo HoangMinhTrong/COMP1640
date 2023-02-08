dee84f340368157a004c8beb06a966a575cdfb8b
Trong Le <Trong Le@DESKTOP-8UJ2AMA>
2023-02-08 21:30:30 +0700

    setup base project

---
+++ COMP1640/Extentions/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace COMP1640.Extentions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+            var t = configuration.GetConnectionString("Local");
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseNpgsql(configuration.GetConnectionString("Local"));
+            });
+
+            return services;
+        }
+    }
+}
---
+++ Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Infrastructure
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+       : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetAssembly(typeof(ApplicationDbContext)));
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        private void OnModelCreatingPartial(ModelBuilder model)
+        {
+
+        }
+    }
+}
