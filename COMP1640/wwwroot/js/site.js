d34020961a81f1861b1eadabce0bcd5fbb60f9c6
Tuns <tuanmoitap@gmail.com>
2023-02-15 16:17:30 +0700

    Add User Popup, Implement UI

--- COMP1640/wwwroot/js/site.js
+++ COMP1640/wwwroot/js/site.js
@@ -1,4 +1,15 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿//Popup Add User
+var modal = document.getElementById("addUserModal");
+var btn = document.getElementById("addUserBtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+span.onclick = function () {
+    modal.style.display = "none";
+}
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
