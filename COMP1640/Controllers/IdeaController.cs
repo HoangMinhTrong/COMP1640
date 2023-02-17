9b4d0395a8f61ff46fc9c29ac59e1cbda8b4ae1c
Ngoc Tong <ngoctong3107@gmail.com>
2023-02-18 00:33:42 +0700

    erp-14-setup create idea

---
+++ COMP1640/Controllers/IdeaController.cs
@@ -0,0 +1,31 @@
+﻿using COMP1640.Services;
+using COMP1640.ViewModels.Idea.Requests;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP1640.Controllers
+{
+    public class IdeaController : Controller
+    {
+        private readonly IdeaService _ideaService;
+
+        public IdeaController(IdeaService ideaService)
+        {
+            _ideaService = ideaService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateIdeaRequest request)
+        {
+            await _ideaService.CreateIdeaAsync(request);
+            return View();
+        }
+
+
+    }
+}
---
+++ COMP1640/ViewModels/Idea/Requests/CreateIdeaRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP1640.ViewModels.Idea.Requests
+{
+    public class CreateIdeaRequest
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Content { get; set; }
+        [Required]
+        public bool IsAnonymous { get; set; }
+    }
+}
