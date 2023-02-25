4bf7b23170fd7ef56f75ef2801aa069c9a400d1e
tuan le <letualttt@gmail.com>
2023-02-25 16:37:20 +0700

    feat: view and filter ideas at homepage

--- COMP1640/Controllers/HomeController.cs
+++ COMP1640/Controllers/HomeController.cs
@@ -1,19 +1,62 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using COMP1640.Services;
+using COMP1640.Utils;
+using COMP1640.ViewModels.Common;
+using Microsoft.AspNetCore.Mvc;
 
 namespace COMP1640.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IdeaService _ideaService;
+        private readonly CategoryService _categoryService;
+        
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IdeaService ideaService, CategoryService categoryService)
         {
             _logger = logger;
+            _ideaService = ideaService;
+            _categoryService = categoryService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index(GetIdeaIndexRequest request)
         {
-            return View();
+            var ideas = await _ideaService.GetIdeaIndexAsync(request);
+            
+            var response = new IdeaIndexResponse()
+            {
+                IdeaIndexItems = ideas,
+                Categories = await _categoryService.GetCategoryPicklistAsync(),
+                SortOptionPicklist = IdeaIndexOption.SortOptionPicklist,
+                
+                // Comment sort option switch since just sort by descending
+                // PopularSortParm = request.SortOption == IdeaIndexOption.PopularIdeaSort
+                //     ? IdeaIndexOption.PopularIdeaSortDesc
+                //     : IdeaIndexOption.PopularIdeaSort,
+                // LatestIdeaSortParm = request.SortOption == IdeaIndexOption.LatestIdeaSort
+                //     ? IdeaIndexOption.LatestIdeaSortDesc
+                //     : IdeaIndexOption.LatestIdeaSort,
+                // LatestCommentSortParm = request.SortOption == IdeaIndexOption.LatestCommentSort
+                //     ? IdeaIndexOption.LatestCommentSortDesc 
+                //     : IdeaIndexOption.LatestCommentSort,
+                // ReactionSortParm = request.SortOption == IdeaIndexOption.ReactionSort
+                //     ? IdeaIndexOption.ReactionSortDesc 
+                //     : IdeaIndexOption.ReactionSort,
+                
+                CurrentSearchString = request.SearchString,
+                CurrentCategoryFilter = request.FilterOption,
+                CurrentSort = request.SortOption,
+                PaginationInfo = new PaginationInfo
+                {
+                    ActualPage = request.PageNumber ?? 1,
+                    TotalItems = ideas.TotalItems,
+                    ItemsPerPage = ideas.Count,
+                    TotalPages = ideas.TotalPages,
+                    Next = ideas.HasNextPage,
+                    Previous = ideas.HasPreviousPage
+                }
+            };
+            return View(response);
         }
 
         public IActionResult Privacy()
---
+++ COMP1640/ViewModels/Common/PaginationInfo.cs
@@ -0,0 +1,11 @@
+namespace COMP1640.ViewModels.Common;
+
+public class PaginationInfo
+{
+    public int TotalItems { get; set; }
+    public int ItemsPerPage { get; set; }
+    public int ActualPage { get; set; }
+    public int TotalPages { get; set; }
+    public bool Previous { get; set; }
+    public bool Next { get; set; }
+}
