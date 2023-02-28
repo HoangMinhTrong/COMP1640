b53437a62fca69884858e7e634b261dca4e17876
tronglt20 <tronglt2001@gmail.com>
2023-02-28 21:29:41 +0700

    config AWS S3

---
+++ Domain/Attachment.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Base;
+
+namespace Domain
+{
+    public class Attachment : TenantAuditEntity<int>
+    {
+        public Attachment()
+        {
+
+        }
+
+        public string Name { get; set; }
+        public string KeyName { get; set; }
+        public float Size { get; set; }
+        public string Extension { get; set; }
+
+        public virtual ICollection<IdeaAttachment> IdeaAttachments { get; set; } = new HashSet<IdeaAttachment>();
+    }
+}
---
+++ Domain/IdeaAttachment.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain
+{
+    public class IdeaAttachment
+    {
+        public IdeaAttachment()
+        {
+
+        }
+
+        public int IdeaId { get; set; }
+        public int AttachmentId { get; set; }
+
+        public virtual Attachment Attachment { get; set; }
+        public virtual Idea Idea { get; set; }
+    }
+}
