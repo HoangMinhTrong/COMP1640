ad15053e28d88d5e22f9fa0d903df6102fc2ab43
kieuvy29 <vy10b3.thptgl@gmail.com>
2023-03-08 11:13:22 +0700

    ERP-82: Move all ideas from Idea page to homepage

--- COMP1640/Controllers/HomeController.cs
+++ COMP1640/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using COMP1640.Services;
 using COMP1640.ViewModels.Common;
+using COMP1640.ViewModels.Idea.Requests;
 using Microsoft.AspNetCore.Mvc;
 using Utilities.Helpers;
 
@@ -19,9 +20,42 @@ namespace COMP1640.Controllers
             _categoryService = categoryService;
         }
 
-        public async Task<IActionResult> Index(GetIdeaIndexRequest request)
+        [HttpGet]
+        public async Task<IActionResult> Index([FromQuery] GetIdeaIndexRequest request)
         {
-            return View("Index");
+            var ideas = await _ideaService.GetIdeaIndexAsync(request);
+
+            var response = new IdeaIndexResponse()
+            {
+                IdeaIndexItems = ideas,
+                SortOptionPicklist = EnumHelper.GetSelectListItems<IdeaIndexSortingEnum>(),
+                Categories = await _categoryService.GetCategoryPicklistAsync(),
+                CurrentSearchString = request.SearchString,
+                CurrentCategoryFilter = request.CategoryFilterOption,
+                CurrentSort = request.SortOption,
+                PaginationInfo = new PaginationInfo
+                {
+                    ActualPage = request.PageNo,
+                    TotalItems = ideas.TotalItems,
+                    ItemsPerPage = ideas.Count,
+                    TotalPages = ideas.TotalPages,
+                    Next = ideas.HasNextPage,
+                    Previous = ideas.HasPreviousPage
+                }
+            };
+            return View("Index", response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateIdeaRequest request)
+        {
+            if (!ModelState.IsValid) return RedirectToAction("Index", "Home");
+
+            var isSucceed = await _ideaService.CreateIdeaAsync(request);
+            if (isSucceed) return RedirectToAction("Index", "Home");
+
+            ModelState.AddModelError("create_exception", "Failure to create a new idea.");
+            return View();
         }
 
         public IActionResult Privacy()
